1c9f6e486d153f9175f2f6cdf8ea0ab247b06e3d
Aurius Bendikas <aurius.bendikas@hm.com>
2018-02-13 16:52:55 +0100

    Refactored GetBuildConfigurationsFromBranch:
    * Made regexp case insensitive.
    * Reduced complexity.

--- src/main/java/com/trimble/tekla/pojo/Listener.java
+++ src/main/java/com/trimble/tekla/pojo/Listener.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
 import org.codehaus.jackson.map.ObjectMapper;
 
 /**
@@ -26,7 +27,7 @@ public class Listener {
     return this.branchConfig;
   }
 
-  public void setBranchConfig(String branchConfig) {
+  public void setBranchConfig(final String branchConfig) {
     this.branchConfig = branchConfig;
   }
 
@@ -34,15 +35,15 @@ public class Listener {
     return this.cancelRunningBuilds;
   }
 
-  public void setCancelRunningBuilds(Boolean cancelRunningBuilds) {
+  public void setCancelRunningBuilds(final Boolean cancelRunningBuilds) {
     this.cancelRunningBuilds = cancelRunningBuilds;
   }
-  
+
   public Boolean getTriggerOnPullRequest() {
     return this.triggerOnPullRequest;
   }
 
-  public void setTriggerOnPullRequest(Boolean triggerOnPullRequest) {
+  public void setTriggerOnPullRequest(final Boolean triggerOnPullRequest) {
     this.triggerOnPullRequest = triggerOnPullRequest;
   }
 
@@ -50,7 +51,7 @@ public class Listener {
     return this.downStreamTriggerType;
   }
 
-  public void setDownStreamTriggerType(String downStreamTriggerType) {
+  public void setDownStreamTriggerType(final String downStreamTriggerType) {
     this.downStreamTriggerType = downStreamTriggerType;
   }
 
@@ -58,7 +59,7 @@ public class Listener {
     return this.downStreamUrl;
   }
 
-  public void setDownStreamUrl(String downStreamUrl) {
+  public void setDownStreamUrl(final String downStreamUrl) {
     this.downStreamUrl = downStreamUrl;
   }
 
@@ -85,23 +86,21 @@ public class Listener {
   public void setTargetId(final String targetId) {
     this.targetId = targetId;
   }
-  
+
   public static Listener[] GetBuildConfigurationsFromBranch(final String jsonConfiguration, final String branch) throws IOException {
     final ObjectMapper mapper = new ObjectMapper();
     final Map<String, Listener> listenerMap;
     final List<Listener> configs = new ArrayList<>();
     listenerMap = mapper.readValue(jsonConfiguration, mapper.getTypeFactory().constructParametricType(HashMap.class, String.class, Listener.class));
     for (final Map.Entry<String, Listener> listenerEntry : listenerMap.entrySet()) {
-      final Pattern pattern = Pattern.compile(listenerEntry.getValue().getRegexp());
+      final Pattern pattern = Pattern.compile(listenerEntry.getValue().getRegexp(), Pattern.CASE_INSENSITIVE);
       final Matcher matcher = pattern.matcher(branch);
-      while (matcher.find()) {
-        for (int i = 1; i <= matcher.groupCount(); i++) {
-          listenerEntry.getValue().setBranchConfig(matcher.group(i));
-          configs.add(listenerEntry.getValue());
-        }
+      if (matcher.find()) {
+        listenerEntry.getValue().setBranchConfig(matcher.group(matcher.groupCount()));
+        configs.add(listenerEntry.getValue());
       }
     }
 
     return configs.toArray(new Listener[configs.size()]);
-  } 
+  }
 }
